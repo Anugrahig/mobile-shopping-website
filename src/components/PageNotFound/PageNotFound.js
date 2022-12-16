64e94cc13e663272f72ad074ab1e11cd18c451da
anugrah <anugrahig@gmail.com>
2022-12-16 21:07:17 +0530

    media query modified

--- src/components/PageNotFound/PageNotFound.js
+++ src/components/PageNotFound/PageNotFound.js
@@ -1,7 +1,14 @@
 import React from "react";
 import "./PageNotFound.scss";
+import PgeNotFound from "../../assets/page_not_found.svg";
 const PageNotFound = () => {
-  return <div>PageNotFound</div>;
+  return (
+    <div>
+      <div className="container">
+        <img src={PgeNotFound} alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default PageNotFound;
